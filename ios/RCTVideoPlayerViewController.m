691f27b4ced4a524e9bf912225a69f13ca5055ae
Ahmed Barakat <ahmed.e.barakat1994@Gmail.com>
2018-06-02 14:55:55 +0200

    Fix partial swiping on video shows black screen
    In iOS 11, Apple added a feature to close the Video Player via a swipe gesture. Partial swiping cases viewWillDisappear to be called without viewDidDisappear which cases a black screen.

--- ios/RCTVideoPlayerViewController.m
+++ ios/RCTVideoPlayerViewController.m
@@ -6,33 +6,11 @@
 
 @implementation RCTVideoPlayerViewController
 
-BOOL _viewWillAppearCalled = NO;
-
-- (void)viewDidLoad
-{
-    [super viewDidLoad];
-    _viewWillAppearCalled = NO;
-}
-
-- (void)viewWillAppear:(BOOL)animated
-{
-    [super viewWillAppear:animated];
-    if (_rctDelegate == nil || _viewWillAppearCalled)
-    {
-        [self dismissViewControllerAnimated:YES completion:nil];
-    }
-    _viewWillAppearCalled = YES;
-}
-
 - (void)viewDidDisappear:(BOOL)animated
 {
-    [super viewDidDisappear:animated];
-    [_rctDelegate videoPlayerViewControllerDidDismiss:self];
-}
-
-- (void)viewWillDisappear:(BOOL)animated {
-    [_rctDelegate videoPlayerViewControllerWillDismiss:self];
-    [super viewWillDisappear:animated];
+  [super viewDidDisappear:animated];
+  [_rctDelegate videoPlayerViewControllerWillDismiss:self];
+  [_rctDelegate videoPlayerViewControllerDidDismiss:self];
 }
 
 @end
